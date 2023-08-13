9600110a29df6cac4a6c08b8479228d99ca5471b
blackpanther007007 <sharmahimanshu121663@gmail.com>
2023-08-13 15:00:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Routes, Route,BrowserRouter } from "react-router-dom"
+import Landingpage from './Components/Landingpage';
+import Signup from './Components/signup';
+import Login from './Components/Login';
+import Trips from './Components/Trips';
+import Citydetails from './Components/Citydetails';
+import TravelForm from './Components/Maketrip';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Landingpage/> } />
+        <Route path="Login" element={ <Login/> } />
+        <Route path="Signup" element={ <Signup/> } />
+        <Route path="Trips" element={ <Trips/> } />
+        <Route path="City" element={ <Citydetails/> } />
+        <Route path="/city/Travel" element={ <TravelForm/> } />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
